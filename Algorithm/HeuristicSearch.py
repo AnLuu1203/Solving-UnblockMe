e3abdbd98c277d4f053919515c2389c5453614f2
An Luu <lta.anluu@gmail.com>
2016-10-17 16:46:53 +0700

    switch tab -> spaces, removed some comment

--- Algorithm/HeuristicSearch.py
+++ Algorithm/HeuristicSearch.py
@@ -4,178 +4,149 @@ import random
 
 
 def findBrick_left(state, currentBrick):
-	result = []
+  result = []
 
-	x = currentBrick.position[0]
-	y = currentBrick.position[1] - 1
-	board = state.createBoard(currentBrick.id)
+  x = currentBrick.position[0]
+  y = currentBrick.position[1] - 1
+  board = state.createBoard(currentBrick.id)
 
-	while y >= 0:
-		value = board[x][y]
-		if value != 0:
-			result = [value] + result
-		y -= 1
+  while y >= 0:
+    value = board[x][y]
+    if value != 0:
+      result = [value] + result
+    y -= 1
 
-	return result
+  return result
 
 def findBrick_right(state,currentBrick):
-	result = []
+  result = []
 
-	x = currentBrick.position[0]
-	y = currentBrick.position[1] + currentBrick.length
-	board = state.createBoard(currentBrick.id)
+  x = currentBrick.position[0]
+  y = currentBrick.position[1] + currentBrick.length
+  board = state.createBoard(currentBrick.id)
 
-	while y < 6:
-		value = board[x][y]
-		if value != 0:
-			result = [value] + result
-		y += 1
+  while y < 6:
+    value = board[x][y]
+    if value != 0:
+      result = [value] + result
+    y += 1
 
-	return result
+  return result
 
 def findBrick_top(state,currentBrick):
-	result = []
+  result = []
 
-	x = currentBrick.position[0]
-	y = currentBrick.position[1]
-	board = state.createBoard(currentBrick.id)
+  x = currentBrick.position[0]
+  y = currentBrick.position[1]
+  board = state.createBoard(currentBrick.id)
 
-	while x >= 0:
-		value = board[x][y]
-		if value != 0:
-			result = [value] + result
-		x -= 1
+  while x >= 0:
+    value = board[x][y]
+    if value != 0:
+      result = [value] + result
+    x -= 1
 
-	return result
+  return result
 
 def findBrick_bottom(state, currentBrick):
-	result = []
+  result = []
 
-	x = currentBrick.position[0] + currentBrick.length
-	y = currentBrick.position[1]
-	board = state.createBoard(currentBrick.id)
+  x = currentBrick.position[0] + currentBrick.length
+  y = currentBrick.position[1]
+  board = state.createBoard(currentBrick.id)
 
-	while x < 6:
-		value = board[x][y]
-		if value != 0:
-			result = [value] + result
-		x += 1
+  while x < 6:
+    value = board[x][y]
+    if value != 0:
+      result = [value] + result
+    x += 1
 
-	return result
+  return result
 
 
 def heuristicFunc(state):
-	block = []
-	stack = []
+  block = []
+  stack = []
 
-	mainBrick = None
+  mainBrick = None
 
-	for brick in state.listBrick:
-		if brick.id == 1:
-			mainBrick = brick
-			break
+  for brick in state.listBrick:
+    if brick.id == 1:
+      mainBrick = brick
+      break
 
-	if mainBrick != None:
-		stack = stack + findBrick_right(state,mainBrick)
+  if mainBrick != None:
+    stack = stack + findBrick_right(state,mainBrick)
 
-	while stack != []:
-		currentBrickId = stack.pop()
+  while stack != []:
+    currentBrickId = stack.pop()
 
-		if currentBrickId in block:
-			continue
-		else:
-			block += [currentBrickId]
+    if currentBrickId in block:
+      continue
+    else:
+      block += [currentBrickId]
 
-		currentBrick = state.findBrickById(currentBrickId)
+    currentBrick = state.findBrickById(currentBrickId)
 
-		if currentBrick.isVetical:
-			top = findBrick_top(state,currentBrick)
-			bottom = findBrick_bottom(state,currentBrick)
+    if currentBrick.isVetical:
+      top = findBrick_top(state,currentBrick)
+      bottom = findBrick_bottom(state,currentBrick)
 
-			stack = stack + top
-			stack = stack + bottom
-			
-		else:
-			left = findBrick_left(state,currentBrick)
-			right = findBrick_right(state,currentBrick)
+      stack = stack + top
+      stack = stack + bottom
 
-			stack = stack + left
-			stack = stack + right
-			
-	return len(block)
+    else:
+      left = findBrick_left(state,currentBrick)
+      right = findBrick_right(state,currentBrick)
+
+      stack = stack + left
+      stack = stack + right
+
+  return len(block)
 
 
 def checkState(state, listState):
-	for s in listState:
-		if state.createBoard(0) == s.createBoard(0):
-			return True
-	return False
-
-
-'''	
-def simple_hill_climbing(startState,depth = 100):
-	stack = [startState]
-	visited = []
-
-	while stack != []:
-		currentState = stack.pop()
-
-		if currentState.checkGoal():
-			return currentState
-
-		visited += [currentState]
-
-
-		nextStates = currentState.generateNextStates(currentState.parentMove[0])
-		nextStates = random.sample(nextStates, len(nextStates))
-		for state in nextStates:
-			if checkState(state,visited):
-				continue
-			else:
-				if heuristicFunc(state) <= heuristicFunc(currentState):
-					stack += [state]
-					print(heuristicFunc(state))
-					break;
-
-	return None
-'''
+  for s in listState:
+    if state.createBoard(0) == s.createBoard(0):
+      return True
+  return False
 
 def steepest_ascent_hill_climbing(startState, depth = 500):
-	stack = [startState]
-	visited = []
+  stack = [startState]
+  visited = []
 
-	while stack != []:
-		currentState = stack.pop()
+  while stack != []:
+    currentState = stack.pop()
 
-		if currentState.checkGoal():
-			return currentState
+    if currentState.checkGoal():
+      return currentState
 
-		visited += [currentState]
+    visited += [currentState]
 
 
-		if currentState.depth < depth:
-			nextStates = currentState.generateNextStates(currentState.parentMove[0])
-			nextStates = random.sample(nextStates, len(nextStates))
-			choice = None
+    if currentState.depth < depth:
+      nextStates = currentState.generateNextStates(currentState.parentMove[0])
+      nextStates = random.sample(nextStates, len(nextStates))
+      choice = None
 
-			if nextStates == []:
-				return None
-			else:
-				choice = nextStates[0]
+      if nextStates == []:
+        return None
+      else:
+        choice = nextStates[0]
 
-			for state in nextStates:
-				if checkState(state,visited):
-					continue
-				else:
-					heuristicState = heuristicFunc(state)
-					heuristicChoice = heuristicFunc(choice)
-					if heuristicState <= heuristicChoice:
-						choice = state
+      for state in nextStates:
+        if checkState(state,visited):
+          continue
+        else:
+          heuristicState = heuristicFunc(state)
+          heuristicChoice = heuristicFunc(choice)
+          if heuristicState <= heuristicChoice:
+            choice = state
 
-			if heuristicFunc(choice) <= heuristicFunc(currentState):
-				stack += [choice]
+      if heuristicFunc(choice) <= heuristicFunc(currentState):
+        stack += [choice]
 
-	return None
-	
+  return None
+
 def solveHeuristic(startState):
-	return steepest_ascent_hill_climbing(startState)
+  return steepest_ascent_hill_climbing(startState)
--- State/Board.py
+++ State/Board.py
@@ -2,111 +2,111 @@ import copy
 from State.Brick import *
 
 class Board:
-	#listBrick: danh các khối gạch
+  #listBrick: danh các khối gạch
 
-	def __init__(self, listBrick, parentState = None, parentMove = (0,0), depth = 0):
-		self.listBrick = listBrick
-		self.parentState = parentState
-		self.parentMove = parentMove
-		self.depth = depth
+  def __init__(self, listBrick, parentState = None, parentMove = (0,0), depth = 0):
+    self.listBrick = listBrick
+    self.parentState = parentState
+    self.parentMove = parentMove
+    self.depth = depth
 
-	#khởi tạo mảng trống
-	def initBoard(self):
-		board = [[0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0]]
-		return board
+  #khởi tạo mảng trống
+  def initBoard(self):
+    board = [[0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0]]
+    return board
 
-	#@param:
-	#id: id của khối gạch
-	#return: vẽ các khối gạch trong mảng, trừ khối gạch có id truyền vào
-	#note: vẽ tất cả khối gạch, truyền id = 0
-	def createBoard(self, id):
-		board = self.initBoard()
+  #@param:
+  #id: id của khối gạch
+  #return: vẽ các khối gạch trong mảng, trừ khối gạch có id truyền vào
+  #note: vẽ tất cả khối gạch, truyền id = 0
+  def createBoard(self, id):
+    board = self.initBoard()
 
-		for brick in self.listBrick:
-			if brick.id != id:
-				if brick.isVetical:
-					x = brick.position[0]
-					y = brick.position[1]
+    for brick in self.listBrick:
+      if brick.id != id:
+        if brick.isVetical:
+          x = brick.position[0]
+          y = brick.position[1]
 
-					for i in range(0,brick.length):
-						board[x + i][y] = brick.id
-				else:
-					x = brick.position[0]
-					y = brick.position[1]
+          for i in range(0,brick.length):
+            board[x + i][y] = brick.id
+        else:
+          x = brick.position[0]
+          y = brick.position[1]
 
-					for i in range(0,brick.length):
-						board[x][y + i] = brick.id
+          for i in range(0,brick.length):
+            board[x][y + i] = brick.id
 
-		return board
+    return board
 
-	#in mảng ra console
-	def printBoard(self):
-		for row in self.createBoard(0):
-			print(row)
-
-
-	#sinh ra list các bước di chuyển hợp lệ, trừ khối gạch có id truyền vào
-	def generateNextMoves(self, id):
-		listMove = []
-
-		for brick in self.listBrick:
-			if brick.id != id:
-				board = self.createBoard(brick.id)
-				moves = brick.generateNextMoves(board)
-				listMove += moves
-
-		return listMove
-
-	#sinh ra các trạng thái từ các bước di chuyển hợp lệ
-	def generateNextStates(self, previousId):
-		listState = []
-		for move in self.generateNextMoves(previousId):
-			listState += [self.generateState(move)]
-		return listState
+  #in mảng ra console
+  def printBoard(self):
+    for row in self.createBoard(0):
+      print(row)
 
 
-	def printState(self):
-		lst = self.generateNextState()
-		for state in lst:
-			for brick in state:
-				print(brick.id, brick.isVetical, brick.position, brick.length)
-			print("\n")
+  #sinh ra list các bước di chuyển hợp lệ, trừ khối gạch có id truyền vào
+  def generateNextMoves(self, id):
+    listMove = []
 
-	#kiểm tra goal
-	def checkGoal(self):
-		board = self.createBoard(0)
-		i = 5
-		while (board[2][i] != 1):
-			if board[2][i] != 0:
-				return False
-			i -= 1
-		return True
+    for brick in self.listBrick:
+      if brick.id != id:
+        board = self.createBoard(brick.id)
+        moves = brick.generateNextMoves(board)
+        listMove += moves
 
-	#@param: 
-	#move: bước di chuyển
-	#@return: trạng thái được sinh ra từ move
-	def generateState(self, move):
-		lst = copy.deepcopy(self.listBrick)
-		for i in range(len(lst)):
-			brick = lst[i]
-			if brick.id == move[0]:
-				if brick.isVetical == True:
-					lst[i] = Brick(brick.id, brick.isVetical, (brick.position[0] + move[1],brick.position[1]), brick.length)
-				else:
-					lst[i] = Brick(brick.id, brick.isVetical, (brick.position[0],brick.position[1] + move[1]), brick.length)
-				break
-		return Board(lst,self,move,self.depth + 1)
+    return listMove
 
-	#return danh sách các bước di chuyển
-	def getResultMove(self, board):
-		if board.parentMove == (0,0):
-			return []
-		else:
-			result = self.getResultMove(board.parentState)
-			return result + [board.parentMove]
+  #sinh ra các trạng thái từ các bước di chuyển hợp lệ
+  def generateNextStates(self, previousId):
+    listState = []
+    for move in self.generateNextMoves(previousId):
+      listState += [self.generateState(move)]
+    return listState
 
-	def findBrickById(self,id):
-		for brick in self.listBrick:
-			if brick.id == id:
-				return brick
-		return None
+
+  def printState(self):
+    lst = self.generateNextState()
+    for state in lst:
+      for brick in state:
+        print(brick.id, brick.isVetical, brick.position, brick.length)
+      print("\n")
+
+  #kiểm tra goal
+  def checkGoal(self):
+    board = self.createBoard(0)
+    i = 5
+    while (board[2][i] != 1):
+      if board[2][i] != 0:
+        return False
+      i -= 1
+    return True
+
+  #@param:
+  #move: bước di chuyển
+  #@return: trạng thái được sinh ra từ move
+  def generateState(self, move):
+    lst = copy.deepcopy(self.listBrick)
+    for i in range(len(lst)):
+      brick = lst[i]
+      if brick.id == move[0]:
+        if brick.isVetical == True:
+          lst[i] = Brick(brick.id, brick.isVetical, (brick.position[0] + move[1],brick.position[1]), brick.length)
+        else:
+          lst[i] = Brick(brick.id, brick.isVetical, (brick.position[0],brick.position[1] + move[1]), brick.length)
+        break
+    return Board(lst,self,move,self.depth + 1)
+
+  #return danh sách các bước di chuyển
+  def getResultMove(self, board):
+    if board.parentMove == (0,0):
+      return []
+    else:
+      result = self.getResultMove(board.parentState)
+      return result + [board.parentMove]
+
+  def findBrickById(self,id):
+    for brick in self.listBrick:
+      if brick.id == id:
+        return brick
+    return None
